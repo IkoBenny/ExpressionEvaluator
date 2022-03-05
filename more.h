14695f0d1ffd013603683a5f93dad87c448a92c0
Benny Iko <benny.iko.it@gmail.com>
2022-03-05 00:16:30 -0500

    Changes to be committed: 	new file:   more.h     On branch master 	Changes: 	N/A 	Issues: 	N/A

---
+++ more.h
@@ -0,0 +1,23 @@
+/*Benny Iko
+ * 5/3/2020
+ * Advanced Programming Languages
+ * CMSC 330 7981
+ * more.h
+ * The arithmetic expressions are fully parenthesized infix expressions containing
+ * integer literals and variables. The valid arithmetic operators are +, –, *, /.
+ * Tokens can be separated by any number of spaces.Extended the program so that it
+ * supports relational, logical and conditional expression operators
+*/
+#pragma once
+class More : public SubExpression
+{
+public:
+	More(Expression* left, Expression* right) :
+		SubExpression(left, right)
+	{
+	}
+	int evaluate()
+	{
+		return left->evaluate() > right->evaluate();
+	}
+};
