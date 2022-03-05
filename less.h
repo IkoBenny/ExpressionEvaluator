1e32062907befec418bb44017a010bb6504a54a5
Benny Iko <benny.iko.it@gmail.com>
2022-03-05 00:15:07 -0500

    Changes to be committed: 	new file:   less.h     On branch master 	Changes: 	N/A 	Issues: 	N/A

---
+++ less.h
@@ -0,0 +1,23 @@
+/*Benny Iko
+ * 5/3/2020
+ * Advanced Programming Languages
+ * CMSC 330 7981
+ * less.h
+ * The arithmetic expressions are fully parenthesized infix expressions containing
+ * integer literals and variables. The valid arithmetic operators are +, –, *, /.
+ * Tokens can be separated by any number of spaces.Extended the program so that it
+ * supports relational, logical and conditional expression operators
+*/
+#pragma once
+class Less : public SubExpression
+{
+public:
+	Less(Expression* left, Expression* right) :
+		SubExpression(left, right)
+	{
+	}
+	int evaluate()
+	{
+		return left->evaluate() < right->evaluate();
+	}
+};
